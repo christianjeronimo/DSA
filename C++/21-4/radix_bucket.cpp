2b6ddab802f3be42984d7bfaaecf0e9e43115b8c
Christian Jeronimo <chrstnjeronimo@gmail.com>
2026-07-30 23:09:43 -0700

    Set up parts of the radix_bucket files for Assignment 4, readded test file

---
+++ C++/21-4/radix_bucket.cpp
@@ -0,0 +1,27 @@
+/*
+ * @filename radix_bucket.cpp
+ * Part of assignment 4
+ * Sorting in constant time
+ */
+
+#include "radix_bucket.h"
+#include <cmath>
+#include <vector>
+
+namespace {
+  constexpr int buckets = 9;
+
+  int digit_value(number, index) {
+
+  }
+
+  std::vector<int> rbsort(std::vector<int> &arr) {
+
+  }
+}
+
+void sort(std::vector<int> &arr) {
+  if (!arr.empty()) {
+    rbsort(arr);
+  }
+}
---
+++ C++/21-4/radix_bucket.h
@@ -0,0 +1,10 @@
+/*
+ * @filename radix_bucket.h
+ * Part of assignment 4
+ * Sorting algorithim in constant time
+ */
+
+#pragma once
+#include <vector>
+
+void sort(std::vector<int> &arr);
