d5475eda2fe8f9391672b034b2d1552085927f0a
Christian Jeronimo <chrstnjeronimo@gmail.com>
2026-08-02 17:27:06 -0700

    made the basic setup for the functions in the header file

---
+++ C++/21-5/hashtable.h
@@ -0,0 +1,33 @@
+/*
+ * @filename hashtable.h
+ * Part of assignment 5
+ * Hash table data structure
+ */
+
+#pragma once
+#include <string>
+#include <list>
+#include <memory>
+#include <utility>
+
+class HashTable {
+private:
+  struct Record {
+    int key;
+    std::string value;
+    Record(int k, std::string v) : key(k), value(v)  {}
+  };
+
+  int capacity;
+  const double c = 0.618034;
+  std::list<std::unique_ptr<Record>>* table;
+  int hash(int key);
+  std::pair<int i, int j> find(int key);
+public:
+  HashTable(int cap = 100);
+  ~HashTable();
+  void insert(int key, std::string value);
+  void search(int key);
+  void remove(int key);
+  void clear();
+};
