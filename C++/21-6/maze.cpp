1406797528e51fc9836fa5770b7e5240152165a2
Christian Jeronimo <chrstnjeronimo@gmail.com>
2026-08-30 13:18:55 -0700

    Provided code for some of the functions in maze.cpp, not tested but hopefully working

--- C++/21-6/maze.cpp
+++ C++/21-6/maze.cpp
@@ -7,6 +7,7 @@
 #include "maze.h"
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
 Maze::Maze(int n) : n(n) {
   grid = new int[n * n];
@@ -28,11 +29,35 @@ Maze::~Maze() {
 }
 
 int Maze::index(int r, int c) {
-  return 0;
+  if (r < 0 || r >= n || c < 0 || c >= n) {
+    return -1;
+  }
+  
+  return r * n + c;
 }
 
 void Maze::remove_wall(int r1, int c1, int r2, int c2) {
-  return;
+  int dr = std::abs(r2 - r1);
+  int dc = std::abs(c2 - c1);
+  
+  if (dr + dc != 1) return;
+
+  dr = r2 - r1;
+  dc = c2 - c1;
+
+  if (dr == 1) {
+    grid[index(r1, c1)] &= ~2;
+    grid[index(r2, c2)] &= ~8;
+  } else if (dr == -1) {
+    grid[index(r1, c1)] &= ~8;
+    grid[index(r2, c2)] &= ~2;
+  } else if (dc == 1) {
+    grid[index(r1, c1)] &= ~1;
+    grid[index(r2, c2)] &= ~4;
+  } else if (dc == -1) {
+    grid[index(r1, c1)] &= ~4;
+    grid[index(r2, c2)] &= ~1;
+  }
 }
 
 void Maze::generate() {
@@ -40,5 +65,13 @@ void Maze::generate() {
 }
 
 void Maze::print_maze() {
-  return;
+  for (int r = 0; r < n; r++) {
+    for (int c = 0; c < n; c++) {
+      std::cout << std::hex << grid[index(r, c)];
+    }
+
+    std::cout << "\n";
+  }
+
+  std::cout << std::dec;
 }
